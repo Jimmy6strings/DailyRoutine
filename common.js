34f39b4c4fa253b2f6eb6d436ed1e6cb0edeb81e
Jimmy6strings <jamescorleym@yahoo.com>
2015-09-22 11:24:23 -0500

    Update common.js

--- common.js
+++ common.js
@@ -5,3 +5,8 @@ var calCounter = function(numCal) {
 	}
 	else {alert("Keep Eating!")};
 };
+var breakfastArray = [granolaParfait: 330, oatmeal: 145, baconAndEggs: 600, pancakes: 700];
+var excerciseArray = [jogging: 500, biking: 500, swimming: 600, weights: 500, sports: 800];
+var lunchArray = [tunaSalad: 400, spinachSalad: 350, turkeyWrap: 500, burrito: 550, pizza: 700];
+var hangoutArray = [waterPark: 300, bicycling: 300, playground: 150, videogames: 0, television: 0];
+var dinnerArray = [pasta: 800, chickenAndBroccoli: 600, meatloaf: 700, pizza: 700, takeOut: 1000];
